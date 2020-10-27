c7b03c958200b9505b5ddcd12eaaa1c393c3182a
Neftcode <arevalocdev@outlook.com>
2020-10-27 17:18:06 -0500

    actualizar crud medicamentos

--- src/Controller/Admin/MedicamentoCrudController.php
+++ src/Controller/Admin/MedicamentoCrudController.php
@@ -4,6 +4,7 @@ namespace App\Controller\Admin;
 
 use App\Entity\Medicamento;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 
 class MedicamentoCrudController extends AbstractCrudController
 {
@@ -12,14 +13,16 @@ class MedicamentoCrudController extends AbstractCrudController
         return Medicamento::class;
     }
 
-    /*
     public function configureFields(string $pageName): iterable
     {
         return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
+            // IdField::new('id'),
+            TextField::new('codigo_atc', 'Código ATC'),
+            TextField::new('nombre'),
+            TextField::new('principio_activo', 'Principio activo'),
+            TextField::new('concentracion', 'Concentración'),
+            TextField::new('forma_farmaceutica', 'Forma farmacéutica'),
         ];
     }
-    */
+    
 }
